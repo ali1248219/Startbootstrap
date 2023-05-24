74b56669e8c2e15113af84100e9fd4f53eb48251
FJ00 <77333830+ali1248219@users.noreply.github.com>
2023-05-24 08:34:26 +0700

    DONE
    btw the FETCH is not really necessary

--- web.py
+++ web.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template
 import requests
 from fetch import Fetch
 
-data_api = requests.get('https://api.npoint.io/76605f216bae8649fb68').json()
+data_api = requests.get('https://api.npoint.io/9a55829c83c3641b7737').json()
 data_object = []
 for data in data_object:
-    data_obj = Fetch(data['id'], data['title'], data['body'], data['subtitle'])
+    data_obj = Fetch(data['id'], data['title'], data['body'], data['subtitle'] , data['author'], data['date'])
     data_object.append(data_obj)
-
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html' , all_data=data_api)
 
 
 @app.route('/about.html')
@@ -30,10 +29,10 @@ def contact():
 @app.route('/post/<int:index>')
 def posts(index):
     request_post = None
-    for data in data_object:
+    for data in data_api:
         if data['id'] == index:
             request_post = data
-    return render_template('post.html', data=request_post)
+    return render_template('post.html', post=request_post)
 
 
 if __name__ == "__main__":
